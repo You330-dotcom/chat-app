9fda1f22c2c929c178920d67112954fe486440ac
You330-dotcom <taikokaiyou@gmail.com>
2026-07-12 21:18:13 +0900

    add flask-sock

--- app.py
+++ app.py
@@ -181,4 +181,5 @@ def logout():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run(host="https://chat-app-jhld.onrender.com", port=5000, debug=False, threaded=False)
+    
